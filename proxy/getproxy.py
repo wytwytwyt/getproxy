41f51ba556ef640d90ef145a23dbabd14bcf7c9d
wyt <wyt960507@Gmail.com>
2017-08-24 11:46:44 +0800

    ver2.01

--- proxy/getproxy.py
+++ proxy/getproxy.py
@@ -15,7 +15,8 @@ class Item(object):
 
 class crawl(object):
 	def __init__(self):
-		self.starturl = 'http://www.kuaidaili.com/ops/proxylist/'
+		print('initing')
+		self.starturl = 'http://www.kuaidaili.com/free/inha/'
 		self.urls = self.get_urls()
 		self.filename = 'proxy.txt'
 		self.proxy_list = self.get_proxy(self.urls)
@@ -26,10 +27,12 @@ class crawl(object):
 		'''
 		url list
 		'''
+
 		urls = []
-		for x in range(1,10):
+		for x in range(1,3):
 			url = self.starturl + str(x)
 			urls.append(url)
+		print('get urls  success')
 		return urls
 
 	def get_proxy(self,urls):
@@ -38,23 +41,42 @@ class crawl(object):
 
 		'''
 		browser = webdriver.PhantomJS()
+
 		proxy_list = []
 		for url in urls:
+			print('browsering' + url)
+
 			browser.get(url)
 			browser.implicitly_wait(3)
 
 			#find proxy
-			elements = browser.find_elements_by_xpath('//tdbody/tr')
+			print('finding elements')
+			elements = browser.find_elements_by_xpath('//tbody/tr')
+			
 
 			for element in elements:
+				print('dealing element')
+
 				item = Item()
-				item.ip = element.find_elements_by_xpath('./td[1]').text
-				item.port = element.find_elements_by_xpath('./td[2]').text
-				item.anontmous = element.find_elements_by_xpath('./td[3]').text
-				item.type = element.find_elements_by_xpath('./td[4]').text
-				item.get_post = element.find_elements_by_xpath('./td[5]').text
-				item.localadress = element.find_elements_by_xpath('./td[6]').text
-				item.speed = element.find_elements_by_xpath('./td[7]').text
+				item.ip = element.find_element_by_xpath('./td[1]').text
+				print('ip get')
+				item.port = element.find_element_by_xpath('./td[2]').text
+				print('port get')
+
+				item.anontmous = element.find_element_by_xpath('./td[3]').text
+				print('anontmous get')
+
+				item.type = element.find_element_by_xpath('./td[4]').text
+				print('type get')
+
+				item.get_post = element.find_element_by_xpath('./td[5]').text
+				print('get_post get')
+
+				item.localadress = element.find_element_by_xpath('./td[6]').text
+				print('localadress get')
+				
+				item.speed = element.find_element_by_xpath('./td[7]').text
+				print(' speed get')
 				print(item.ip)
 				proxy_list.append(item)
 		browser.quit()
@@ -74,6 +96,7 @@ class crawl(object):
 				f.write(item.type + '\t')
 				f.write(item.get_post + '\t')
 				f.write(item.speed + '\n\n')
+			print('saveing success')
 
 if __name__ == '__main__':
 	Get = crawl()
